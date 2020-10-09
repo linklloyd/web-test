162b5704be0d1e9538bdd34002dd1eddd782d332
Luis <luis@iMac.local>
2020-10-09 13:05:53 -0500

    Primer versión de web

---
+++ script.js
@@ -0,0 +1,14 @@
+function winner() {
+  document.getElementById("prize").style.fontFamily="Comic sans MS";
+  document.getElementById("prize").innerHTML = "You win 2 second less of life";
+}                   
+
+function info() {
+    alert("This page was created by Luis Alfredo Abarca Molina <Linklloyd>")
+}
+
+console.log ("Why you can see this?")
+//Why someone will see this secret text 
+
+/* Just testing comments
+and also here */
